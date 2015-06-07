legendre derivatives value corrected, tensorproduct made faster,directory creating issue resolved, running on cluster, providing good looking plots

kernels.py
@@ -4,6 +4,7 @@ import PLegendre
 import os
 import tensorproduct
 import time
+import errno
 
 nnode=10
 ndiv =960
@@ -13,31 +14,40 @@ coreid = int(os.environ['PBS_VNODENUM'])
 coreid = (coreid-nodeid*ppn)
 omega_per_node=ndiv//nnode
 
-print 'nodeid and coreid are ',nodeid,coreid
+#print 'nodeid and coreid are ',nodeid,coreid
 
 sin=np.sin
 cos=np.cos
 pi=np.pi
 
-nlat = 288    
+ellmax=128
+nlat = (ellmax+ppn)-ellmax%ppn    
 nlon = 2 * nlat
-ellmax = 256
 
 src_lat = pi / 2 
-src_lon = pi / 2 
+src_lon = pi / 6 
 rcv_lat = pi / 2 
 rcv_lon = pi / 4 
 
-path = '/scratch/samrat/kernel/greens/'
+path = '/scratch/samrat/kernel/greens_7-06-15_10Xdamping/'
 
-directory_kSS = '/scratch/samrat/kernel/greens/sound_speed_individual_256_ells/'
-if not os.path.exists(directory_kSS):
-    os.makedirs(directory_kSS)
+directory_kSS = '/scratch/samrat/kernel/sound_speed_longitudal_blocks_128_ells/'
+if not os.path.isdir(directory_kSS):
+    try:
+        os.makedirs(directory_kSS)
+    except OSError, e:
+        if e.errno != errno.EEXIST:
+            raise e
+        pass
 
-directory_kD = '/scratch/samrat/kernel/greens/density_individual_256_ells/'
-if not os.path.exists(directory_kD):
-    os.makedirs(directory_kD)
-
+directory_kD = '/scratch/samrat/kernel/density_longitudal_blocks_128_ells/'
+if not os.path.isdir(directory_kD):
+    try:
+        os.makedirs(directory_kD)
+    except OSError, e:
+        if e.errno != errno.EEXIST:
+            raise e
+        pass
 nr = len(np.load(os.path.join(path,'omega-0100.npz'))['r'])
 latfull = np.atleast_2d(np.linspace (0, pi,   nlat)).T
 lonfull = np.atleast_2d(np.linspace (0, 2*pi, nlon))
@@ -126,10 +136,10 @@ def sum_over_l_for_omega(iOmega):
 	filename = 'omega-'+str(iOmega).zfill(4)+'.npz'
 	npzfile = os.path.join(path,filename) 
 	gdata=np.load(npzfile) 
-	t2=time.time() 
+	#~ t2=time.time() 
 	for ell in xrange(ellmax+1):
 		#~ t2=time.time()        
-		norm=(2*ell+1)/(4*pi)
+		norm=(2.*ell+1.0)/(4*pi)
 		
 		tensorproduct.outer_and_add_density(
 						gdata['xisrc_denkernel'][ell],LP_src_deriv[0,ell],kd_xisrc,
@@ -146,7 +156,7 @@ def sum_over_l_for_omega(iOmega):
 						norm
 						)
 		#~ print 'time taken for ell ',ell,' is ',(time.time() - t2),'sec','for iOmega',iOmega,'on proc no. ',coreid
-	print 'time taken ',(time.time() - t2),'sec','for iOmega',iOmega,'on proc no. ',coreid	
+	#~ print 'time taken ',(time.time() - t2),'sec','for iOmega',iOmega,'on proc no. ',coreid	
 	kd_indv = np.real(kd_xisrc * kd_xircv + kd_psrc * kd_prcv)
 	kd_xisrc,kd_xircv,kd_psrc,kd_prcv=None,None,None,None
 				 

main.py
@@ -3,10 +3,16 @@ import numpy as np
 import mi_cython as mc
 import os
 from scipy import interpolate
+import errno
 
-save_path = "/scratch/samrat/kernel/greens_June_6_3xDamping/"
-if not os.path.exists(save_path):
-    os.makedirs(save_path)
+save_path = "/scratch/samrat/kernel/greens_7-06-15_10Xdamping"
+if not os.path.isdir(save_path):
+	try:
+		os.makedirs(save_path)
+	except OSError, e:
+		if e.errno != errno.EEXIST:
+			raise e
+		pass
 codedir="/home/samrat/SoKer"
 
 pi = np.pi
@@ -41,7 +47,7 @@ nu = np.linspace( numin, numax, ndiv) * 1e-3
 
 dampingfile=os.path.join(codedir,"m585q.4816")
 dmpnu = np.loadtxt(dampingfile,usecols= [2])*1e-6
-fwhm = 3.0*np.loadtxt(dampingfile,usecols= [4])*1e-6 #increasing the damping to avoid resolution problem
+fwhm = np.loadtxt(dampingfile,usecols= [4])*1e-6 *1e1 
 
 # Sort the damping array before interpolating onto our grid
 points = zip(dmpnu, fwhm)

sum_on_cluster.py
@@ -1,13 +1,15 @@
+from __future__ import division
 import numpy as np
 import os
 import time
 t=time.time()
-directory_kSS = '/scratch/samrat/kernel/greens/sound_speed_individual_256_ells/'
-directory_kD = '/scratch/samrat/kernel/greens/density_individual_256_ells/'
+
+directory_kSS = '/scratch/samrat/kernel/sound_speed_longitudal_blocks_128_ells/'
+directory_kD = '/scratch/samrat/kernel/density_longitudal_blocks_128_ells/'
 nnode=10
 ppn=24
 nr=798
-nlat=288
+nlat=144
 nlon=2*nlat
 k_density=np.zeros((nr,nlat,1),dtype=float)
 k_soundspeed=np.zeros((nr,nlat,1),dtype=float)
@@ -24,7 +26,7 @@ for coreid in xrange(ppn):
 		print 'Now loading density for nodeid and coreid ',nodeid,coreid,'in time',(time.time()-t1),'sec'
 	k_density=np.dstack((k_density,density_kernel_sum))
 k_density=np.delete(k_density,0,2)
-np.savez(os.path.join(directory_kD,'density_K'),k_density=k_density)
+np.savez(os.path.join(directory_kD,'kernel_d'),kernel_d=k_density)
 k_density=None
 for coreid in xrange(ppn):
 	ss_kernel_sum=np.zeros((nr,nlat,nlon//ppn),dtype=float)
@@ -38,6 +40,6 @@ for coreid in xrange(ppn):
 		print 'Now loading soundspeed for nodeid and coreid ',nodeid,coreid,'in time',(time.time()-t2),'sec'
 	k_soundspeed=np.dstack((k_soundspeed,ss_kernel_sum))
 k_soundspeed=np.delete(k_soundspeed,0,2)
-np.savez(os.path.join(directory_kSS,'Sspeed_K'),k_soundspeed=k_soundspeed)
+np.savez(os.path.join(directory_kSS,'kernel_s'),kernel_s=k_soundspeed)
 k_soundspeed=None
 print 'total time taken is ',time.time()-t,'sec'